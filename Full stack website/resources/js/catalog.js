686b47e2bddf18823ad5324befac21f3f3d3cb14
HooeyPooey <hooeypooey@lifetime.oregonstate.edu>
2018-07-17 03:13:02 -0700

    upload a singke page website project

---
+++ Full stack website/resources/js/catalog.js
@@ -0,0 +1,22 @@
+function openBook(evt, BookName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tab-content" and hide them
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(BookName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
